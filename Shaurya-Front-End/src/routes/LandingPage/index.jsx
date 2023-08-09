662d751afbdc1065d25545af507329adcbaf56b0
dev-064 <divyanshujha112@gmail.com>
2023-08-09 23:00:28 +0530

    repository initialised

---
+++ Shaurya-Front-End/src/routes/LandingPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const LandingPage = () => {
+  return (
+    <>
+      Landing Page
+    </>
+  );
+};
+
+export default LandingPage;
---
+++ Shaurya-Front-End/src/routes/index.js
@@ -0,0 +1,3 @@
+export { default as LandingPage } from "./LandingPage/index.jsx";
+export { default as EventsPage } from "./EventsPage/index.jsx";
+export { default as TeamsPage } from "./TeamsPage/index.jsx";
