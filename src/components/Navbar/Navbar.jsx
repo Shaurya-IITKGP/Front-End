a2ee737d036459d80259cead5c521ce91d926e41
Rohan <rohan@zenskar.com>
2023-09-01 20:44:47 +0530

    Navbar fixed, sort of

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,74 +1,91 @@
-import React, { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
-import s from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import shauryaLogo from "../../assets/shauya22-.png";
 
-import logo from "../../assets/shauya22-.png";
-const Menu = () => (
-  <>
-    <div className="  text-white">
-      <div className="">
-        <h1 className="my-2   ">
-          <Link to="/events">Events</Link>
-        </h1>
-      </div>
-      <div className="">
-        <h1 className="my-2 ">
-          <Link to="/sponsors">Sponsors</Link>
-        </h1>
-
-        <h1 className="my-2  ">
-          <Link to="/teams">Team</Link>
-        </h1>
-      </div>
-     
-    </div>
-  </>
-);
+// TODO : Add KGP Logo
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
 
+  const location = useLocation();
+  const mobileNavRef = useRef(null);
+
+  useEffect(() => {
+    setToggleMenu(false);
+  }, [location]);
+
   return (
-    <div className={"flex bg-black  text-orange-500  "}>
-      <div className="flex items-center justify-between  w-full">
-        <div>
-          <h1>
-            <img className={"w-20 ml-12 mb-2" + s.logo1} src={logo} alt="" />
-          </h1>
+    <>
+      {toggleMenu && (
+        <div className="bg-black opacity-80 absolute z-50 w-full h-full flex justify-center items-center">
+          <div
+            ref={mobileNavRef}
+            className={
+              "w-full h-full relative flex justify-center items-center"
+            }
+          >
+            <div className="text-orange-500 absolute right-4 top-4">
+              <RiCloseLine onClick={() => setToggleMenu(false)} size={40} />
+            </div>
+            <div className="flex flex-col gap-4 px-10 py-10">
+              <Link
+                className="text-orange-500 font-bold text-2xl hover:text-white transition-all"
+                to="/events"
+              >
+                EVENTS
+              </Link>
+              <Link
+                className="text-orange-500 font-bold text-2xl hover:text-white transition-all"
+                to="/sponsors"
+              >
+                SPONSORS
+              </Link>
+              <Link
+                className="text-orange-500 font-bold text-2xl hover:text-white transition-all"
+                to="/teams"
+              >
+                TEAM
+              </Link>
+            </div>
+          </div>
         </div>
-        <div
-          className={"flex gap-20 items-center font-mono font-semibold pr-10"}
-        >
-          <h1 className={"my-6  hover:text-white hidden md:block"}>
+      )}
+      <div
+        className={
+          "flex items-center w-full md:justify-between bg-black shadow-sm shadow-[#090A0B] md:px-16 px-4 py-4 text-orange-500"
+        }
+      >
+        <div className="md:border-none md:pr-0 md:mr-0 pr-4 mr-4 border-r-2 border-white">
+          <img
+            className={"w-12"}
+            src={shauryaLogo}
+            alt="SHAURYA - IIT Kharagpur"
+          />
+        </div>
+        <div className={"flex gap-10 items-center font-mono font-semibold"}>
+          <h1 className={"transition-all hover:text-white hidden md:block"}>
             <Link to="/events">EVENTS</Link>
           </h1>
-          <h1 className={"my-6 hover:text-white hidden md:block"}>
+          <h1 className={"transition-all hover:text-white hidden md:block"}>
             <Link to="/sponsors">SPONSORS</Link>
           </h1>
-          <h1 className={"my-6  hover:text-white hidden md:block"}>
+          <h1 className={"transition-all hover:text-white hidden md:block"}>
             <Link to="/teams">TEAM</Link>
           </h1>
-
-          
+        </div>
+        <div>
+          <img className="" src="" alt="IIT Kharagpur" />
+        </div>
+        <div className="md:hidden flex justify-end w-full">
+          <RiMenu3Line
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+            size={30}
+          />
         </div>
       </div>
-      <div className={"absolute top-0 right-0 mt-7 " + s.hammenu}>
-        {toggleMenu ? (
-          <RiCloseLine
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(false)}
-          />
-        ) : (
-          <RiMenu3Line
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(true)}
-          />
-        )}
-        {toggleMenu && <Menu />}
-      </div>
-    </div>
+    </>
   );
 }
