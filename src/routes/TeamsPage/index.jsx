97e735e1871674f7c6d4ff54258358701ffdf14c
Somya <isomya13@gmail.com>
2023-09-15 06:44:34 +0530

    team page responsive

--- src/routes/TeamsPage/index.jsx
+++ src/routes/TeamsPage/index.jsx
@@ -1,23 +1,34 @@
+import { Heading } from "@chakra-ui/react";
+import TEAM_DATA from "../../CardData/TEAM_DATA";
 import { TeamCard } from "../../components/TeamCard";
 
 const TeamsPage = () => {
   return (
-    <div className="grid pb-20 justify-items-center md:grid-cols-3 gap-10 px-12 py-10">
-      <TeamCard
-        designation={"Web Head"}
-        name={"Name"}
-        image={"https://i.ibb.co/JC4skS0/team-animate.jpg"}
-      />
-      <TeamCard
-        designation={"Design Head"}
-        name={"Name"}
-        image={"https://i.ibb.co/JC4skS0/team-animate.jpg"}
-      />
-      <TeamCard
-        designation={"Core Head"}
-        name={"Name"}
-        image={"https://i.ibb.co/JC4skS0/team-animate.jpg"}
-      />
+    <div className="py-10 px-10">
+      <div className="grid md:grid-cols-3 mb-10 gap-8 justify-items-center">
+        <Heading className="md:col-span-3 text-3xl text-white text-bold text-center">
+          EXECUTIVE HEADS
+        </Heading>
+        {TEAM_DATA["EXECUTIVE HEADS"].map((person) => (
+          <TeamCard
+            designation={person?.designation}
+            name={person?.name}
+            image={person?.image || "https://i.ibb.co/JC4skS0/team-animate.jpg"}
+          />
+        ))}
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 justify-items-center">
+        <Heading className="md:col-span-3 text-3xl text-white text-bold text-center">
+          HEADS
+        </Heading>
+        {TEAM_DATA["HEADS"].map((person) => (
+          <TeamCard
+            designation={person?.designation}
+            name={person?.name}
+            image={person?.image || "https://i.ibb.co/JC4skS0/team-animate.jpg"}
+          />
+        ))}
+      </div>
     </div>
   );
 };
