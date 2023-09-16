afb0acbf320cac0096a7085dffb2e76819e5787d
Rohan Barsagade <rbarsagade101@gmail.com>
2023-09-16 10:53:44 +0530

    backend integration and small ui changes

--- src/routes/EventsPage/index.jsx
+++ src/routes/EventsPage/index.jsx
@@ -23,11 +23,11 @@ const EventsPage = () => {
 
   return (
     <>
-      <div className="md:px-20 w-full px-10 pb-20 max-w-[1440px] mx-auto">
-        <Heading color="white" className="py-9 px-3">
+      <div className="md:px-5 w-full px-5 pb-20 max-w-[1440px] mx-auto">
+        <Heading color="white" className="py-9 text-center px-3">
           EVENTS
         </Heading>
-        <div className="flex md:justify-around justify-center items-center flex-wrap gap-5 mb-3">
+        <div className="w-fit grid gap-8 md:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center mx-auto">
           {EVENTS.map((event, index) => (
             <Card
               key={index}
