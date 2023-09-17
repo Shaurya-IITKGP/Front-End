01b881ae2453718cac9c5a02cbdf842d516957c4
Somya <isomya13@gmail.com>
2023-09-17 19:09:16 +0530

    added some social media links

--- src/CardData/TEAM_DATA.jsx
+++ src/CardData/TEAM_DATA.jsx
@@ -34,6 +34,9 @@ const TEAM_DATA = {
       image: null,
       designation: "General Secretary: Sports and Games",
       email: "soujanya4013@gmail.com",
+      facebook:"",
+      linkedin: "",
+      instagram:""
     }, 
     {
       name: "SSVKSS Jyothiraditya",
@@ -46,6 +49,9 @@ const TEAM_DATA = {
       image: null,
       designation: "Institute Girl Sports Nominee",
       email: "mshreya1210@gmail.com",
+      linkedin: "https://www.linkedin.com/in/shreyamishra1210",
+      instagram: "https://www.instagram.com/shreya.mishra_12/",
+      facebook: "https://www.facebook.com/profile.php?id=100074610314302&mibextid=D4KYlr"
     },
   ],
   HEADS: [
@@ -54,6 +60,8 @@ const TEAM_DATA = {
       image: null,
       designation: "Sponsorship Head",
       email: "adityagaikwad@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100024188513667&mibextid=ZbWKwL",
+      linkedin: "https://www.linkedin.com/in/aditya-g-gaikwad-155a67229",
     },
     {
       name: "Akshat Dilip Lade",
@@ -66,42 +74,60 @@ const TEAM_DATA = {
       image: akula,
       designation: "Logistics Head",
       email: "akulatejaswini23@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/tejaswini-akula-a87884224",
+      instagram: "https://instagram.com/__tejaswini.akula?igshid=OGQ5ZDc2ODk2ZA==",
+      facebook: "https://www.facebook.com/profile.php?id=100073662512238"
     },
     {
       name: "Anushka Singh",
       image: anushka,
       designation: "Accomodation and Guest Reception Head",
       email: "anushkasingh@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100075405404571",
+      linkedin: "https://www.linkedin.com/in/anushka-singh-285482227?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVTWzVMydQAyj14D61CR7eA%3D%3D"
     },
     {
       name: "Archie Avirati",
       image: archie,
       designation: "Events Head",
       email: "archieavirati@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100074750037795&mibextid=b06tZ0",
+      linkedin: "https://www.linkedin.com/in/archieavirati"
     },
     {
       name: "Atharva chilwarwar",
       image: atharva,
       designation: "Publicity and Marketing Head",
       email: "Chilwarwar.atharva@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100074784741856",
+      linkedin: "https://www.linkedin.com/in/atharva-chilwarwar-91899822b/"
     },
     {
       name: "Chalamalla Sahithi",
       image: sahithi,
       designation: "Sponsorship Head",
-      email: "sahithi@kgpian.iitkgp.ac.in",
+      email: "sahithi.chalamalla@shaurya-iitkgp.ac.in",
+      facebook: "https://www.facebook.com/sahithi.ch.5055?mibextid=ZbWKwL",
+      instagram: "https://instagram.com/sahithi_chalama1la?igshid=OGQ5ZDc2ODk2ZA==",
+      linkedin: "https://www.linkedin.com/in/sahithi-chalamalla-461a0a231"
     },
     {
       name: "Dhiya Mariam Thomas",
       image: dhiya,
       designation: "Accomodation and Guest Reception Head",
-      email: "",
+      email: "dhiyamt2003@gmail.com",
+      linkedin: "https://www.linkedin.com/in/dhiya-mariam-thomas-3078ab24a",
+      facebook: "https://www.facebook.com/dhiya.mariamthomas?mibextid=2JQ9oc",
+      instagram: "https://instagram.com/d_hi_yaaa?igshid=OGQ5ZDc2ODk2ZA=="
     },
     {
       name: "Gauransh Agarwal",
       image: gauransh,
       designation: "Design Head",
       email: "gauransh@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/gauransh-agarwal-087b39231/",
+      instagram: "https://www.instagram.com/gauransh_agarwal_/",
+      facebook: "https://www.facebook.com/gauransh.agarwal.50"
     },
     {
       name: "Harshvardhan Malla",
@@ -114,30 +140,41 @@ const TEAM_DATA = {
       image: himanshu,
       designation: "Events Head",
       email: "golusaini786@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/himanshu-saini-7b5246230",
+     instagram: "https://instagram.com/golu_kkc786?utm_source=qr&igshid=YzU1NGVlODEzOA==",
+     facebook: "https://m.facebook.com/profile.php/?id=100008771947144&name=xhp_nt__fb__action__open_user"
     },
     {
       name: "Jatin Motwani",
       image: jatin,
       designation: "Finance Head",
       email: "jatinmotwani@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/hitesh.motwani.7127/",
+      linkedin: "https://www.linkedin.com/in/jatin-motwani-684b2022a/"
     },
     {
       name: "Lokesh Agarwala",
       image: lokesh,
       designation: "Finance Head",
       email: "lokesh_agarwala@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100074403405010",
+      linkedin: "http://www.linkedin.com/in/lokeshagarwala"
     },
     {
       name: "Neeraj Patel",
       image: neeraj,
       designation: "Logistics Head",
       email: "neeraj.patel@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100075286177788",
+      linkedin: "https://www.linkedin.com/in/neerazpatel"
     },
     {
       name: "Nikhil Bharat Rajani",
       image: nikhil,
       designation: "Events Head",
       email: "nikhilrajani309@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100083735865227",
+      linkedin: "https://www.linkedin.com/in/nikhil-bharat-rajani"
     },
     {
       name: "Nimish Gadge",
@@ -156,12 +193,16 @@ const TEAM_DATA = {
       image: prashant,
       designation: "Publicity and Marketing Head",
       email: "prashant@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/profile.php?id=100013963752708&mibextid=ZbWKwL",
+      linkedin: "https://www.linkedin.com/in/prashant-tripathi-a17365224"
     },
     {
       name: "Priyanshu Shaw",
       image: priyanshu,
       designation: "Events Head",
       email: "priyanshushaw2807@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/priyanshu.shaw.9404",
+      linkedin: "https://www.linkedin.com/in/priyanshu-shaw-59a852235/"
     },
     {
       name: "Raghuvar Srivastava",
@@ -186,12 +227,18 @@ const TEAM_DATA = {
       image: siddharth,
       designation: "Publicity and Markenting Head",
       email: "siddharthsabhari@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/siddharth-sabhari-shankar-144745214/",
+      instagram: "https://www.instagram.com/siddharth.sabhari.shankar/",
+      facebook: "https://www.facebook.com/siddharth.sabharishankar/"
     },
     {
       name: "Sanyam Jhuria",
       image: sanyam,
       designation: "Sponsorship Head",
       email: "sanyamjhuria807@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/sanyam-jhuria-4aaa68229/",
+      instagram: "https://instagram.com/sanyam_jhuria?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+      facebook: "https://www.facebook.com/profile.php?id=100077283706839&mibextid=ZbWKwL"
     },
     {
       name: "Somyajeet Gupta Chowdhury",
@@ -204,6 +251,9 @@ const TEAM_DATA = {
       image: vaibhav,
       designation: "Design Head",
       email: "vaibhav.joshi@kgpian.iitkgp.ac.in",
+      linkedin: "https://www.linkedin.com/in/vaibhav-joshi-220841230/",
+      instagram: "https://www.instagram.com/jaibhav_voshi",
+      facebook: "https://www.facebook.com/profile.php?id=100075648814522"
     },
     {
       name: "Vijeta Singh",
@@ -216,12 +266,17 @@ const TEAM_DATA = {
       image: yashwanth,
       designation: "Publicity and Markenting Head",
       email: "kyashwanth30@kgpian.iitkgp.ac.in",
+      facebook: "https://www.facebook.com/yashwanthkumarkallepalli",
+      linkedin: "https://www.linkedin.com/in/-yashwanth"
     },
     {
       name: "Shubham Dilawar",
       image: shubham,
       designation: "Design Head",
-      email: "",
+      email: "shubhamdilawar23@gmail.com",
+      linkedin: "https://www.linkedin.com/in/shubham-dilawar-256206239",
+      instagram: "https://instagram.com/shubhamdilawar?igshid=NTc4MTIwNjQ2YQ==",
+      facebook: "https://www.facebook.com/profile.php?id=100075526052654&mibextid=D4KYlr"
     },
   ],
 };
