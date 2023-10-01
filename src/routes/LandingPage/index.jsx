247f5cd3c96ea88a9c392d7a1d3ef2bc90b0b196
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2023-10-01 19:35:30 +0530

    Made new footer component, done all the required changes

--- src/routes/LandingPage/index.jsx
+++ src/routes/LandingPage/index.jsx
@@ -3,11 +3,13 @@ import s from "./landing.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../AppContext/AppContext";
-import footer_logo from "../../assets/logo-shaurya.png"
+
 import sportsimg1 from "../../assets/sport-icon-clipart-7-removebg-preview1.png"
 import sportsimg2 from "../../assets/sport-icon-clipart-7-removebg-preview2.png"
 import sportsimg3 from "../../assets/sport-icon-clipart-7-removebg-preview3.png"
-import About from "../AboutPage/About";
+import Footer from "../Footer/Footer";
+
+
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -85,51 +87,10 @@ const LandingPage = () => {
 
 
       <div className="mt-20">
-      <footer className={s.footer}>
-  	 <div className={s.footcontainer}>
-  	 	<div className={s.footrow}>
-  	 		<div className={s.footer_col}>
-  	 			<div className={s.foot_shaurya_logo}><img className={s.foot_shaurya_logo_img} src={footer_logo} alt="" /></div>
-  	 			
-  	 		</div>
-  	 		<div className={s.footer_col}>
-  	 			<h4>Contact Us</h4>
-  	 			<ul className="">
-            <div className="flex-col">
-              <li>
-            
-            <div>
-            <h1 className="text-white">Shreya Mishra</h1>
-              <a className="text-white" href="https://mail.google.com/mail/u/lee@example.org/#compose">mshreya1210@gmail.com</a></div>
-            <div>
-            <h1 className="text-white">Soujanya Nayak</h1>
-              <a className="text-white" href="https://mail.google.com/mail/u/lee@example.org/#compose">soujanya.nayak@shaurya-iitkgp.org</a></div>
-            <div>
-            <h1 className="text-white">SSVKSS Jyothiraditya</h1>
-              <a className="text-white" href="https://mail.google.com/mail/u/lee@example.org/#compose">jyothiraditya29@gmail.com</a></div>
-            </li>
-
-
-
-            </div>
-            
-  	 				
-  	 			</ul>
-  	 		</div>
-  	 		
-  	 		<div class={s.footer_col}>
-  	 			<h4>Follow us</h4>
-  	 			<div class={s.social_links}>
-  	 				<a href="https://www.facebook.com/shauryaiitkgp/"><i className="fab fa-facebook-f"></i></a>
-  	 			
-  	 				<a href="https://www.instagram.com/shaurya.iitkgp/"><i className="fab fa-instagram"></i></a>
-  	 				<a href="https://www.linkedin.com/company/shaurya-iit-kharagpur/"><i className="fab fa-linkedin-in"></i></a>
-  	 			</div>
-  	 		</div>
-  	 	</div>
-  	 </div>
-  </footer>
+        <Footer/>
         
+        
+     
       </div>
      </div> 
 
